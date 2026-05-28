d103db4254d7bf1c4e3a6a6c30797629df677670
RondneyLoiola <rondney.loiola11@gmail.com>
2026-05-28 11:26:36 -0300

    ..

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ import { WhatsAppButton } from '@/components/whatsapp-button'
 
 export default function HomePage() {
   return (
-    <main className="min-h-screen">
+    <main className="min-h-screen overflow-x-hidden">
       <Header />
       <HeroSection />
       <AboutSection />
